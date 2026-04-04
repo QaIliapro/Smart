ba981e2e0cc5049bf338699018699dae9fb4eb87
QaIliapro <gogogo3435@gmail.com>
2026-04-04 15:03:23 +0300

    Fix: отключить ESLint при сборке, TypeScript fix

--- src/app/trade-in/page.tsx
+++ src/app/trade-in/page.tsx
@@ -38,7 +38,7 @@ export default async function TradeInPage() {
         </div>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {products.map((product: typeof products[number], i) => (
+          {products.map((product: typeof products[number], i: number) => (
             <UsedProductCard
               key={product.id}
               product={product}
